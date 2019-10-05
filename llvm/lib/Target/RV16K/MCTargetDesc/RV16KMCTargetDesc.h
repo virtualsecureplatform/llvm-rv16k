8d079316bc54c39de0626d87ddba5e9ce9ebda69
Ushitora Anqou <ushitora@anqou.net>
2019-10-05 16:36:49 +0900

    Update comments about license in source code we wrote for RV16K
    These files are copied and modified from The LLVM Compiler Infrastructure, which is distributed under the University of Illinois Open Source License (see LICENSE.TXT for details). These files are licensed under the same license.

--- llvm/lib/Target/RV16K/MCTargetDesc/RV16KMCTargetDesc.h
+++ llvm/lib/Target/RV16K/MCTargetDesc/RV16KMCTargetDesc.h
@@ -1,16 +1,6 @@
-// Copyright 2019 Ryotaro Banno, Kotaro Matsuoka, Naoki Matsumoto
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// This file is copied and modified from The LLVM Compiler Infrastructure, which
+// is distributed under the University of Illinois Open Source License (see
+// LICENSE.TXT for details). This file is licensed under the same license.
 
 #ifndef LLVM_LIB_TARGET_RV16K_MCTARGETDESC_RV16KMCTARGETDESC_H
 #define LLVM_LIB_TARGET_RV16K_MCTARGETDESC_RV16KMCTARGETDESC_H
